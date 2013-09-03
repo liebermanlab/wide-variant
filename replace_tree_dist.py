ae1c17e6d9659e0b8ebfb009458ae7d88a1c9f6e
Chung, Hattie <hattiechung@gmail.com>
2013-09-03 14:37:59 -0400

    python script for replacing .tree numbers with SNP integer distances

--- replace_tree_dist.py
+++ replace_tree_dist.py
@@ -1,32 +1,39 @@
 import os, sys, re, math
 
 # param
-distscale = 0.00901
+lung_distscale = 0.00901
 
-def replace_dist(line):
+def replace_dist(line, distscale):
     pattern = '(?<=:)\d+.\d+'
     # calculate values to substitute with 
     matches = re.findall(pattern, line) 
-    m_sub = [str(int(math.ceil(float(m)/distscale))) for m in matches] 
+    m_sub = [str(int(math.floor(float(m)/float(distscale)))) for m in matches]
     
     # replace 
     subs = iter(m_sub) 
     newline = re.sub(pattern, lambda _: next(subs), line) 
     return newline
 
-def main(in_file, out_file): 
+def main(in_file, out_file, dist_scale): 
     fh = open(in_file, 'rU')
     output = open(out_file, 'w') 
     for line in fh:
         # get new line to write
-        newline = replace_dist(line)     
+        newline = replace_dist(line, dist_scale)     
         output.write(newline) 
     output.close()
 
 if __name__ == '__main__': 
-    if len(sys.argv[1:]) is not 2: 
+    '''
+    USAGE: python replace_tree_dist.py [infile] [outfile] [distscale]
+    '''
+    if len(sys.argv[1:]) < 2: 
         print 'Please specify input tree and output file name' 
         sys.exit(1)
+    elif len(sys.argv[1:]) is 2: 
+        distscale = lung_distscale
+
     inputfile = sys.argv[1] 
     outfile = sys.argv[2] 
-    main(inputfile, outfile) 
+    distscale = sys.argv[3]
+    main(inputfile, outfile, distscale) 
