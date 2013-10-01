c06548a9ea961ce749c4cbe455f55edcca8ac2e6
Chung, Hattie <hattiechung@gmail.com>
2013-10-01 15:08:14 -0400

    calculating dist within site

--- calculate_dist_within_site.m
+++ calculate_dist_within_site.m
@@ -1,17 +1,24 @@
-function all_distances = calculate_dist_within_site(fixedmuts, site_isolates)
+function [all_distances, dist_matrix] = calculate_dist_within_site(fixedmuts, site_isolates)
     % HC 7/26/2013
     % given site_isolates, an array with positions for isolate #s in
     % mut_freq, calculates distribution of pairwise distances
     
-    % calculate all papirwise combinations
+    % calculate all pairwise combinations
     all_pairs = combnk(site_isolates,2); 
     num_pairs = size(all_pairs,1); 
+    
+    % hack to get position for dist_matrix
+    all_pairs_index = combnk(1:length(site_isolates),2); 
+    
     % get distances
-    all_distances = zeros(num_pairs,1); 
+    all_distances = zeros(num_pairs,1);
+    dist_matrix = zeros(length(site_isolates),length(site_isolates)); 
     for i = 1:num_pairs
         pair = all_pairs(i,:); 
+        pair_index = all_pairs_index(i,:); 
         dist = calculate_distance_between_two_isolates(fixedmuts, pair(1), pair(2)); 
-        all_distances(i) = dist; 
+        dist_matrix(pair_index(1), pair_index(2)) = dist; 
+        all_distances(i) = dist;
     end
     
 end
