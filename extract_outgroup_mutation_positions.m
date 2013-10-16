529d0bfec0d5919efb48d94203247099fc9b6845
Chung, Hattie <hattiechung@gmail.com>
2013-10-15 21:22:06 -0400

    script to add outgroup in phylogeny construction

--- extract_outgroup_mutation_positions.m
+++ extract_outgroup_mutation_positions.m
@@ -1,8 +1,12 @@
-function ref_tree_positions = extract_outgroup_mutation_positions(ref_folder, RefGenome, chromosomal_positions)
+function [ref_tree_positions, chr_name] = extract_outgroup_mutation_positions(ref_folder, RefGenome, chromosomal_positions)
     fastafile = [ref_folder '/Reference_Genomes/' RefGenome '/genome.fasta']; 
     
     fprintf('\nGetting outgroup sequences from reference genome\n\t%s\n', fastafile);
     fr = fastaread(fastafile) ;
     fasta_seq = fr.Sequence; 
     ref_tree_positions = fasta_seq(chromosomal_positions); 
+    
+    % HACK (HC 2013/10/04)
+    divider_positions = strfind(fr.Header,'|'); 
+    chr_name = fr.Header(divider_positions(3)+1:divider_positions(4)-1); 
 end
--- make_phylogeny.m
+++ make_phylogeny.m
@@ -1,8 +1,8 @@
 outgroup = 1; 
 mainfolder = '/Volumes/sysbio/KISHONY LAB/illumina_pipeline';
-% RefGenomeNameUnedited = 'Smaltophilia_K279';
-RefGenomeNameUnedited = 'Paeruginosa_DK2'; 
-TreeSampleNames = SampleNames; 
+RefGenomeNameUnedited = 'Smaltophilia_K279';
+% RefGenomeNameUnedited = 'Paeruginosa_DK2'; 
+% TreeSampleNames = SampleNames; 
 all_positions = 1:size(Calls,1); 
 quality_positions = all_positions(MutQual>qual_0);
 quality_positions_chromosomal = p(quality_positions); 
@@ -10,9 +10,12 @@ calls_for_tree = Calls(quality_positions,:);
 
 if outgroup == 1
     % ADD REFERENCE AT THESE POSITIONS
-    outgroup_nts = extract_outgroup_mutation_positions(mainfolder, RefGenomeNameUnedited, quality_positions_chromosomal); 
-    calls_for_tree = [calls_for_tree, outgroup_nts']; 
-    TreeSampleNames{end+1} = 'Outgroup'; 
+    [outgroup_nts, chromosome_name] = extract_outgroup_mutation_positions(mainfolder, RefGenomeNameUnedited, quality_positions_chromosomal); 
+    calls_for_tree = [outgroup_nts', calls_for_tree]; 
+    TreeSampleNames{1} = 'Outgroup'; 
+    TreeSampleNames(2:length(SampleNames)+1) = SampleNames; 
+else
+    TreeSampleNames = SampleNames; 
 end
 
 % generate_parsimony_tree(calls_for_tree, TreeSampleNames); 
@@ -20,3 +23,28 @@ end
 % generate_parsimony_tree(NTs(maNT(sum(mutAF>0,2)>0,:)), SampleNames); 
 % %the generated [timestamp]_out.tree file is best viewed in FigTree
 % fprintf('\nDone with tree\n'); 
+
+%% write [chromosome name, quality_positions_chromosomal, calls for all isolates 
+treecounting_csv_fn = 'test_treecounting/treecounting_chart.csv';
+fid = fopen(treecounting_csv_fn, 'w'); 
+num_muts = size(calls_for_tree,1); 
+num_samples = length(TreeSampleNames); 
+
+% write header
+fprintf(fid, 'chr,pos');
+for s = 1:num_samples
+    fprintf(fid, ',%s', TreeSampleNames{s});
+end
+fprintf(fid,'\n');
+
+% write data 
+for m = 1:num_muts
+    fprintf(fid, '%s,%i', chromosome_name, quality_positions_chromosomal(m)); 
+    for s = 1:num_samples
+        fprintf(fid, ',%s', calls_for_tree(m,s)); 
+    end
+    fprintf(fid, '\n'); 
+end
+
+fclose(fid);
+fprintf('\nDONE!\n');
