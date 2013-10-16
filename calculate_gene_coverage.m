2ea55b551da9e8fb137691b7e5e9793e8c836a23
Chung, Hattie <hattiechung@gmail.com>
2013-10-15 21:24:46 -0400

    differential gene cov pipeline

--- calculate_gene_coverage.m
+++ calculate_gene_coverage.m
@@ -1,17 +1,22 @@
-function [cds_isolates, cds_nonredundant_names, avg_cov, cds_nonredundant] = calculate_gene_coverage(samplenames, cds, genomesize)
+function [gene_cov_all_isolates, gene_err_all_isolates, cov_modes, cds_nonredundant, coverage_per_bp] = calculate_gene_coverage(samplenames, cds, genomesize)
+    
+    % params
+    readlen = 100; 
+
+    % initialize 
     cds_nonredundant_names = cell(0,1); 
-    cds_isolates = zeros(0,length(samplenames)); 
-    arbitrary_threshold = 0.9; 
+    gene_cov_all_isolates = zeros(0,length(samplenames));
+    gene_err_all_isolates = zeros(0,length(samplenames)); 
     
     % load coverage for isolates
-    [coverage_positions, avg_cov] = get_all_covered_genes(samplenames, genomesize); 
+    [coverage_per_bp, cov_modes] = get_all_covered_genes(samplenames, genomesize); 
     
     % annotate coding positions of chromosome
     coding_sequences = cds{1}; 
     cds_nonredundant = coding_sequences; 
-    % initialize 
-
+    
     old_locustag = ''; 
+    
     for c = 1:length(coding_sequences) 
         pos = coding_sequences(c).indices; 
         locustag = coding_sequences(c).locustag; 
@@ -22,19 +27,25 @@ function [cds_isolates, cds_nonredundant_names, avg_cov, cds_nonredundant] = cal
         
         % add CDS entry, if non-redundant
         if ~strcmp(locustag, old_locustag)
-            cur_gene_coverage = sum(coverage_positions(:,gene_begin:gene_end),2);
-            isolates_gene_found = cur_gene_coverage > arbitrary_threshold*gene_len;
-            cds_isolates(end+1,:) = isolates_gene_found; 
+            sum_of_calls_in_gene = coverage_per_bp(:,gene_begin:gene_end); 
+            reads_per_gene = (1./readlen)*sum(sum_of_calls_in_gene,2);
+            
+            % normalize 
+            coverage_per_gene = (readlen./gene_len)*(reads_per_gene./cov_modes); 
+            gene_cov_all_isolates(end+1,:) = coverage_per_gene; 
+            
+            error_per_gene = (readlen./gene_len)*(sqrt(reads_per_gene)./cov_modes); 
+            gene_err_all_isolates(end+1,:) = error_per_gene; 
             
             if ~isempty(gene_name)
                 cds_nonredundant_names{end+1} = gene_name; 
             else
                 cds_nonredundant_names{end+1} = locustag; 
             end
+            
         else
             cds_nonredundant(c) = []; 
         end
-        
         old_locustag = locustag; 
     end
     
