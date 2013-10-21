8157891b8d4a4da585c804d0f3e34575c5643dda
Chung, Hattie <hattiechung@gmail.com>
2013-10-21 18:56:19 -0400

    hattie lung stuff

---
+++ hattie_lung_analysis.m
@@ -0,0 +1,14 @@
+% Lung Specific Analysis
+
+lung_analysis = 0; 
+
+if lung_analysis == 1
+    % plot pairwise distance within site 
+    lung_pairwise_distances(mut_freq, SampleNames); 
+
+    % plot number of unique isolates within and between sites 
+    lung_get_all_clonality(mut_freq, SampleNames); 
+
+    % plot distribution of pairwise distances
+    [within, between, distance_matrix] = plot_all_isolate_pairwise_distances(mut_freq, SampleNames); 
+end
