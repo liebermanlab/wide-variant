2ddd83fe3d4f862e47c79097bfaa916b1d3c3da9
Chung, Hattie <hattiechung@gmail.com>
2013-11-06 15:44:05 -0500

    minor change

--- annotate_mutations_auto_gb.m
+++ annotate_mutations_auto_gb.m
@@ -16,7 +16,10 @@ an = zeros(size(Positions,1),1) ;
 sequences={};
 for i=1:length(Scaf)
     
-    a=Scaf{i};f=find(a=='|',2,'last');
+    a=Scaf{i};
+    % only works for NCBI formatted header names
+    f=find(a=='|',2,'last');
+    
     if f > 1
         fn = a(f(1)+1:f(2)-1) ;
     else
