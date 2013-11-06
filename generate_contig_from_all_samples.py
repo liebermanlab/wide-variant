d3c9f8110de721d810102f2da33f051e024e3373
Chung, Hattie <hattiechung@gmail.com>
2013-11-06 15:15:06 -0500

    small fix for combining all unaligned reads

--- generate_contig_from_all_samples.py
+++ generate_contig_from_all_samples.py
@@ -5,6 +5,7 @@ qname = 'sysbio_12h';
 pathsdir = '/home/hc168/illumina_pipeline'
 outputdir = 'unaligned_contig/'
 hash_length = 31
+min_contig_length = 5000
 
 if __name__ == '__main__': 
     # Step 1. Merge all unaligned reads into 2 master files (FW + RV) 
@@ -20,5 +21,6 @@ if __name__ == '__main__':
             "-fastq", "-shortPaired", "unaligned.all.1.fastq", "unaligned.all.2.fastq"]
     print '\nCalling velveth'
     subprocess.call(velveth_cmds) 
+    
     # Step 3. Run velvetg 
-    velvetg_cmds = [pathsdir+"/velvet_1.2.10/velvetg", outputdir, "-cov_cutoff", "10", "-min_contig_lgth", "500"]
+    velvetg_cmds = [pathsdir+"/velvet_1.2.10/velvetg", outputdir, "-cov_cutoff", "10", "-min_contig_lgth", str(min_contig_length)]
